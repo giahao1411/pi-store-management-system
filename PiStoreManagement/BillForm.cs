b80b27cf3680f70ed25d3c93887c687c06929a4c
unknown <trangiahao498@gmail.com>
2024-10-18 00:25:44 +0700

    minor changes

--- PiStoreManagement/BillForm.cs
+++ PiStoreManagement/BillForm.cs
@@ -2,12 +2,10 @@
 using DTO;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace PiStoreManagement
--- PiStoreManagement/OrderForm.cs
+++ PiStoreManagement/OrderForm.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Net.Sockets;
-using System.Runtime.CompilerServices;
-using System.Security.AccessControl;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web;
 using System.Windows.Forms;
 using BUS;
 using DTO;
