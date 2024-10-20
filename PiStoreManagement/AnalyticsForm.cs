e24e8016adde2013ce6db7844cb5a4c07d251e18
unknown <trangiahao498@gmail.com>
2024-10-21 01:37:39 +0700

    ceiling the total income for y-axis of line chart in analytics

--- PiStoreManagement/AnalyticsForm.cs
+++ PiStoreManagement/AnalyticsForm.cs
@@ -38,21 +38,19 @@ namespace PiStoreManagement
             barChart.Series.Clear();
             pieChart.Series.Clear();
 
-            Series lineSeries = new Series("Income");
-            lineSeries.ChartType = SeriesChartType.Spline;
+            Series lineSeries = new Series("Income") { ChartType = SeriesChartType.Spline };
+            Series barSerires = new Series("Income") { ChartType = SeriesChartType.Column };
+            Series pieSeries = new Series("Income") { ChartType = SeriesChartType.Pie };
+
             lineSeries.BorderWidth = 3;
-            lineChart.Series.Add(lineSeries);
-
-            Series barSerires = new Series("Income");
-            barSerires.ChartType = SeriesChartType.Column;
             barSerires.Color = Color.ForestGreen;
-            barChart.Series.Add(barSerires);
 
-            Series pieSeries = new Series("Income");
-            pieSeries.ChartType = SeriesChartType.Pie;
+            lineChart.Series.Add(lineSeries);
+            barChart.Series.Add(barSerires);
             pieChart.Series.Add(pieSeries);
 
             string xAxisTitle = "";
+            decimal maxIncome = 0;
 
             foreach (string data in incomeData)
             {
@@ -63,23 +61,38 @@ namespace PiStoreManagement
                     string textPart = dataPart[0];
                     double totalIncome = double.Parse(dataPart[1]);
 
-                    // date value
-                    if (period == "Last 7 days" || period == "Day")
+                    maxIncome = Math.Max(maxIncome, (decimal)totalIncome);
+
+                    // last 7 days
+                    if (period == "Last 7 days")
                     {
                         if (DateTime.TryParse(textPart, out DateTime dateValue))
                         {
-                            lineSeries.Points.AddXY(dateValue.ToString("MM/dd/yyyy"), totalIncome);
-                            barSerires.Points.AddXY(dateValue.ToString("MM/dd/yyyy"), totalIncome);
-                            pieSeries.Points.AddXY(dateValue.ToString("MM/dd/yyyy"), totalIncome);
+                            lineSeries.Points.AddXY(dateValue.ToString("MM/dd"), totalIncome);
+                            barSerires.Points.AddXY(dateValue.ToString("MM/dd"), totalIncome);
+                            pieSeries.Points.AddXY(dateValue.ToString("MM/dd"), totalIncome);
 
                             xAxisTitle = "Date";
                         }
                     }
                     
+                    // day
+                    if (period == "Day")
+                    {
+                        if (DateTime.TryParse(textPart, out DateTime dateValue))
+                        {
+                            lineSeries.Points.AddXY(dateValue.ToString("hh:mm"), totalIncome);
+                            barSerires.Points.AddXY(dateValue.ToString("hh:mm"), totalIncome);
+                            pieSeries.Points.AddXY(dateValue.ToString("hh:mm"), totalIncome);
+
+                            xAxisTitle = "Time";
+                        }
+                    }
+
                     // week
                     if (period == "Week")
                     {
-                        int dayOfWeek;
+                        int dayOfWeek = 1;
                         if (int.TryParse(textPart, out dayOfWeek))
                         {
                             string dayName = Enum.GetName(typeof(DayOfWeek), dayOfWeek - 1);
@@ -122,14 +135,26 @@ namespace PiStoreManagement
                 }
             }
 
-            //lineChart.ChartAreas[0].AxisY.Interval = (lineChart.ChartAreas[0].AxisY.Maximum - lineChart.ChartAreas[0].AxisY.Minimum) / 10; 
+            // ceiling the total income of y-axis
+            lineChart.ChartAreas[0].AxisY.Maximum = Math.Ceiling((double)maxIncome * 1.1);
+            barChart.ChartAreas[0].AxisY.Maximum = Math.Ceiling((double)maxIncome * 1.1);
+            pieChart.ChartAreas[0].AxisY.Maximum = Math.Ceiling((double)maxIncome * 1.1); 
 
             lineChart.ChartAreas[0].AxisX.Title = xAxisTitle;
+            barChart.ChartAreas[0].AxisX.Title = xAxisTitle;
+
             lineChart.ChartAreas[0].AxisY.Title = "Income";
+
+            pieChart.Titles.Clear();
             lineChart.Titles.Clear();
+            barChart.Titles.Clear();
+
             lineChart.Titles.Add($"{period} Income Report");
+            barChart.Titles.Add($"{period} Income Report");
 
             // realign the x-axis of bar chart
+            lineChart.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            lineChart.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
             barChart.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
             barChart.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
 
@@ -149,7 +174,7 @@ namespace PiStoreManagement
                     query = "SELECT CAST(BillDate AS DATE) AS Text, SUM(TotalPrice) AS TotalIncome FROM Bill WHERE BillDate >= DATEADD(DAY, -7, GETDATE()) GROUP BY CAST(BillDate AS DATE) ORDER BY Text";
                     break;
                 case "Day":
-                    query = "SELECT CAST(BillDate AS DATE) AS Text, SUM(TotalPrice) AS TotalIncome FROM Bill WHERE CAST(BillDate AS DATE) = CAST(GETDATE() AS DATE) GROUP BY CAST(BillDate AS DATE)";
+                    query = "SELECT CAST(BillDate AS DATETIME) AS Text, TotalPrice AS TotalIncome FROM Bill WHERE CAST(BillDate AS DATE) = CAST(GETDATE() AS DATE);";
                     break;
                 case "Week":
                     query = "SELECT DATEPART(WEEKDAY, BillDate) AS Text, SUM(TotalPrice) AS TotalIncome FROM Bill WHERE DATEDIFF(WEEK, 0, BillDate) = DATEDIFF(WEEK, 0, GETDATE()) GROUP BY DATEPART(WEEKDAY, BillDate) ORDER BY Text;";
